6bd12c0df3f1aca4c0fdba1753452547b7d60ad2
0918nobita <nobita.0918@gmail.com>
2024-10-01 18:58:57 +0900

    特定期間内の日付を順に出力するサンプルを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import dayjs from 'dayjs';
 import ja from 'dayjs/locale/ja';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore.js';
 
+dayjs.extend(isSameOrBefore);
 dayjs.locale(ja);
 
-const today = dayjs();
+const startDay = dayjs('2024-10-01');
+const endDay = dayjs('2025-01-20');
 
-console.log(today.format('YYYY-MM-DD HH:mm:ss'));
+for (
+    let currentDay = startDay;
+    currentDay.isSameOrBefore(endDay);
+    currentDay = currentDay.add(1, 'day')
+) {
+    console.log(currentDay.format('YYYY-MM-DD'));
+}
